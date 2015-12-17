018808d83391d5b4fd230671bb0be0cd3d09013f
MilesLin <jkmejkme@yahoo.com.tw>
2015-12-17 23:55:47 +0800

    partail class

---
+++ ClassDemo/Dog1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassDemo
+{
+    public partial class Dog
+    {
+        public void Walk()
+        {
+            Console.WriteLine("Walk");
+        }
+    }
+}
---
+++ ClassDemo/Dog2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassDemo
+{
+    public partial class Dog
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eat");
+        }
+    }
+}
