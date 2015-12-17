545f8564c850ee6a4956b1622c9f225dbcff0c2b
MilesLin <jkmejkme@yahoo.com.tw>
2015-12-17 22:42:03 +0800

    ClassAndStruct

--- ClassAndStruct/Program.cs
+++ ClassAndStruct/Program.cs
@@ -1,15 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClassAndStruct
+﻿namespace ClassAndStruct
 {
-    class Program
+    using System;
+
+    /// <summary>
+    /// 結構
+    /// </summary>
+    public struct TheStruct
     {
-        static void Main(string[] args)
+        public int x;
+    }
+
+    /// <summary>
+    /// 類別
+    /// </summary>
+    public class TheClass
+    {
+        public int x;
+    }
+
+    internal class Program
+    {
+        /// <summary>
+        /// 改變傳遞過來結構的值
+        /// </summary>
+        /// <param name="s">傳遞結構</param>
+        private static void StructTaker(TheStruct s)
         {
+            s.x = 5;
+        }
+
+        /// <summary>
+        /// 改變傳遞過來類別的值
+        /// </summary>
+        /// <param name="c">傳遞類別</param>
+        private static void ClassTaker(TheClass c)
+        {
+            c.x = 5;
+        }
+
+        private static void Main(string[] args)
+        {
+            #region Struct
+
+            TheStruct st = new TheStruct();
+
+            st.x = 1;
+
+            StructTaker(st);
+
+            Console.WriteLine(st.x); // output : 1
+
+            #endregion Struct
+
+            #region Class
+
+            TheClass cs = new TheClass();
+
+            cs.x = 1;
+
+            ClassTaker(cs);
+
+            Console.WriteLine(cs.x); // output : 5
+
+            #endregion Class
         }
     }
 }
