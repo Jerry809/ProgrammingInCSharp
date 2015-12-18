abdb3f98cad3f2a33d521aaec9ffb361b31a3447
MilesLin <jkmejkme@yahoo.com.tw>
2015-12-19 01:06:09 +0800

    方法簽章、具名參數

---
+++ MethodSignature/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSignature
+{
+    public class Customer
+    {
+        public int Points { get; private set; }
+
+        // 公開方法、回傳值int 、名稱AddPoint、傳入一個參數、傳入型別int
+        public int AddPoints(int pointsToAdd) 
+        {
+            this.Points += pointsToAdd;
+            return Points;
+        }
+
+        // 公開方法、回傳值int 、名稱AddPoint、傳入兩個參數、傳入型別int
+        public int AddPoints(int pointsToAdd, int masPoints)
+        {
+            Points += pointsToAdd;
+            if (this.Points > masPoints )
+            {
+                this.Points = masPoints;
+            }
+
+            return this.Points;
+        }
+
+        public void Pay(int amount, string name = "Kevin", string method = "Cash") 
+        {
+            // TODO
+        }
+    }
+}
---
+++ MethodSignature/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSignature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            
+            // 用具名的方式，可直接傳入第三個參數
+            customer.Pay(100, method : "Credit");
+
+            // 一定要傳參數，且會自動對應到該方法
+            customer.AddPoints(10);
+
+            // 一定要傳參數，且會自動對應到該方法
+            customer.AddPoints(10, 20);
+
+        }
+    }
+}
