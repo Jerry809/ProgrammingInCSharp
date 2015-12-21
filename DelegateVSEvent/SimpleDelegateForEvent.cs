d5ee32fd208a85be063e807ae6eeb23731d2e924
MilesLin <jkmejkme@yahoo.com.tw>
2015-12-21 22:37:10 +0800

    基本的event

---
+++ DelegateVSEvent/SimpleDelegateForEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateVSEvent
+{
+    public class SimpleDelegateForEvent
+    {
+        public event EventHandler MyEvent;
+
+        public void DomSomething() 
+        {
+            if (MyEvent != null)
+            {
+                MyEvent(this, EventArgs.Empty);
+            }
+        }
+
+    }
+}
