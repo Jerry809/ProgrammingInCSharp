82e12f8b69919c08e99bcafd00c4c5e49f8d8f39
MilesLin <jkmejkme@yahoo.com.tw>
2015-12-29 00:22:49 +0800

    HowToGetTypedata

---
+++ HowToGetTypedata/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowToGetTypedata
+{
+    public class Animal
+    {
+        public Animal()
+        {
+            this.NumberOfLegs = 4;
+        }
+
+        public Animal(int number)
+        {
+            this.NumberOfLegs = number;
+        }
+
+        public int NumberOfLegs { get; set; }
+
+        public string SayWoo() 
+        {
+            return "Woo";
+        }
+    }
+}
---
+++ HowToGetTypedata/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowToGetTypedata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicInstance a = new DynamicInstance();
+            a.Main();
+            //var dog = new Animal() { NumberOfLegs = 4 };
+
+            //// 方法一 編譯時期取得
+            //Type t1 = typeof(Animal);            
+
+            //// 方法二 執行時期取得
+            //Type t2 = dog.GetType();
+
+            //Console.WriteLine(t2.Name);
+            //// output: Animal
+
+            //Console.WriteLine(t2.FullName);
+            //// output: HowToGetTypedata.Animal
+
+            //Console.WriteLine(t2.Assembly);
+            //// output: HowToGetTypedata, Version = 1.0.0.0,
+            ////         Culture=neutral, PublicKeyToken=null
+
+        }
+    }
+}
