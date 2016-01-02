489a297d6701d52677ca70283277558a78b5b83a
MilesLin <jkmejkme@yahoo.com.tw>
2016-01-03 00:41:22 +0800

    WeakReference

---
+++ WeakReferences/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeakReferences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeakReference w;
+
+            using (var hugeClass = new HugeClass())
+            {
+                w = new WeakReference(hugeClass);
+            }
+            
+            // 直接從記憶體取回HugeClass
+            HugeClass originalHugeClass = w.Target as HugeClass;
+
+            originalHugeClass.Main(); // output : I'm Huge
+
+
+        }
+    }
+
+    class HugeClass : IDisposable
+    {
+        public void Main() { Console.WriteLine("I'm Huge"); }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
