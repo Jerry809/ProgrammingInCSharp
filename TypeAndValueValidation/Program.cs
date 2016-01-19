4c46af853c621fb0ec6da57977056fb3eca173ae
MilesLin <jkmejkme@yahoo.com.tw>
2016-01-19 22:56:14 +0800

    TypeAndValueValidation

---
+++ TypeAndValueValidation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeAndValueValidation
+{
+    class Program
+    {
+        private static string _name;
+
+        public static string Name
+        {
+            get { return _name; }
+            set 
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("不得為null");
+                }
+
+                if (value == Name)
+                {
+                    throw new ArgumentException("重複的值");
+                }
+
+                _name = value; 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Name = null;
+        }
+    }
+}
