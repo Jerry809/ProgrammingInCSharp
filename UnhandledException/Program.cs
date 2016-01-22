3396fa78440e7ede9c9dfd740332dd9feeffddaa
MilesLin <jkmejkme@yahoo.com.tw>
2016-01-22 22:47:50 +0800

    UnhandledException

---
+++ UnhandledException/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnhandledException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TestThrowException();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("不管有沒有Exception都會跑到這段finally");
+            }
+        }
+
+        private static void TestThrowException() 
+        {
+            decimal a = 5;
+            decimal b = 0;
+            decimal result = a / b;
+
+            Console.WriteLine(result);
+        }
+    }
+}
