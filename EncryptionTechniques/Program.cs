e0d50412a5c3d832963035b251a9cbd46eebb27a
MilesLin <jkmejkme@yahoo.com.tw>
2016-01-23 23:04:42 +0800

    EncryptionTechniques

---
+++ EncryptionTechniques/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionTechniques
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            
+            var fileName = Path.GetFileName("test.txt");
+
+            // 加密
+            File.Encrypt(fileName);
+            
+            // 解密
+            //File.Decrypt(fileName);
+                       
+        }
+    }
+}
