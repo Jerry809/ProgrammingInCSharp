a725953429f29e4b2145527ad5cf6ad35aed9df3
MilesLin <jkmejkme@yahoo.com.tw>
2016-01-27 02:30:46 +0800

    SymmetricEncryption

---
+++ SymmetricEncryption/AsymmetricSample.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+
+namespace SymmetricEncryption
+{
+    public class AsymmetricSample
+    {
+        public byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo)
+        {
+            byte[] encryptedData;
+            // 建立RSACryptoServiceProvider
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                // 傳入公鑰加密
+                RSA.ImportParameters(RSAKeyInfo);
+
+                // 執行加密
+                // 參數一:傳入要被加密的資料
+                // 參數二:如果使用 OAEP 填補 (只適用於執行 Microsoft Windows XP 或更新版本的電腦) 執行直接 System.Security.Cryptography.RSA
+                //        加密，則為 true，如果使用 PKCS#1 v1.5 填補，則為 false。
+                encryptedData = RSA.Encrypt(DataToEncrypt, false);
+            }
+            // 回傳用 公鑰 加密後的資料
+            return encryptedData;
+        }
+
+        public byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo)
+        {
+            byte[] decryptedData;
+            // 建立RSACryptoServiceProvider
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                // 傳入私鑰解密
+                RSA.ImportParameters(RSAKeyInfo);
+
+                // 執行解密
+                // 參數一:傳入要被解密的資料
+                // 參數二:如果使用 OAEP 填補 (只適用於執行 Microsoft Windows XP 或更新版本的電腦) 執行直接 System.Security.Cryptography.RSA
+                //        加密，則為 true，如果使用 PKCS#1 v1.5 填補，則為 false。
+                decryptedData = RSA.Decrypt(DataToDecrypt, false);
+            }
+            // 回傳解密後的資料
+            return decryptedData;
+        }
+    }
+}
---
+++ SymmetricEncryption/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetricEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SymmetricSample ss = new SymmetricSample();
+
+            //string text = "SensitiveData";
+            
+            //// 指定演算法
+            //AesCryptoServiceProvider key = new AesCryptoServiceProvider();
+            
+            //// 加密
+            //byte[] encryptedText = ss.Encrypt(text, key);            
+            //Console.WriteLine(Convert.ToBase64String(encryptedText));
+
+            //// 解密
+            //string decryptedText = ss.Decrypt(encryptedText, key);
+            //Console.WriteLine(decryptedText);
+
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+            
+            string text = "SensitiveData";
+
+            AsymmetricSample ays = new AsymmetricSample();
+            
+            //指定演算法
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();            
+
+            
+            byte[] plaintext = ByteConverter.GetBytes(text);
+            // 加密
+            // 傳入 1.要被加密的資料 2.傳入公鑰
+            byte[] encryptedtext = ays.RSAEncrypt(plaintext, RSA.ExportParameters(false));
+            Console.WriteLine(ByteConverter.GetString(encryptedtext));                
+
+            // 解密
+            // 傳入 1.要被解密的資料 2.傳入私鑰
+            byte[] decryptedtex = ays.RSADecrypt(encryptedtext, RSA.ExportParameters(true));
+            Console.WriteLine(ByteConverter.GetString(decryptedtex));            
+
+        }
+
+    }
+}
