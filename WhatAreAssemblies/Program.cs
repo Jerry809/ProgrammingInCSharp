f5cb58c2ec4d6d80ff36fd2e2d000e0dae8e4bab
MilesLin <jkmejkme@yahoo.com.tw>
2016-02-04 01:48:50 +0800

    Assemblies

---
+++ WhatAreAssemblies/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using TestAssembly;
+
+namespace WhatAreAssemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 c = new Class1();
+
+            c.Run();
+        }
+    }
+}
---
+++ WinMDDemo/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinMDDemo
+{
+    public sealed class DemoClass
+    {
+
+    }
+}
