afde9e232c7044b7323863d1c7c4cc7e1cd2357b
MilesLin <jkmejkme@yahoo.com.tw>
2016-02-20 02:00:57 +0800

    AsyncAndAwait

---
+++ AsyncAndAwait/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwait
+{
+    public class Main
+    {
+        public async Task<int> GetSumAsync(int a, int b) 
+        {            
+            Console.WriteLine("步驟 2 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);
+            int result = await GetDoubleSumAsync(a, b);
+            Console.WriteLine("步驟 4 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);            
+            return result;
+        }
+
+        private async Task<int> GetDoubleSumAsync(int a, int b) 
+        {            
+            return await Task.Run(() => 
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("步驟 3 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);
+                return a + a + b + b; 
+            });
+        }
+    }
+}
---
+++ AsyncAndAwait/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main main = new Main();
+            Console.WriteLine("步驟 1 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);
+            Task<int> sum = main.GetSumAsync(1, 10);
+            Console.WriteLine("步驟 5 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("結果:" + sum.Result); // output : 22
+            Console.WriteLine("步驟 6 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);
+
+            Console.Read();
+        }
+
+    }
+}
